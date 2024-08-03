4196ed0dc054ad7c3ecdb326b181a5497aa8b671
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-08-03 19:41:00 +0100

    Config tailwindcss content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts"],
+  content: [
+    './src/**/*.{html,ts',
+    './pages/**/*.{html,ts}',
+    './components/**/*.{html,ts}',
+    './index.html'
+  ],
   theme: {
     extend: {},
   },
