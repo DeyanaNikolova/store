00b91934eb5182f0a340b652096ccdc7b7da8808
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2024-08-04 12:50:30 +0100

    Add methods

--- src/app/pages/home/components/products-header.component.ts
+++ src/app/pages/home/components/products-header.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-products-header',
   standalone: true,
-  imports: [ ],
+  imports: [MatCardModule, MatMenuModule, MatIconModule],
   templateUrl: './products-header.component.html',
-  // styleUrl: './home.component.css'
+  styleUrl: './products-header.component.css',
 })
 export class ProductsHeaderComponent {
+  @Output() columnsCountChange = new EventEmitter<number>();
+  sort = 'desc';
+  itemsShowCount = 12;
 
+  onSortUpdated(newSort: string): void {
+    this.sort = newSort;
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
+  }
+
+  onColumnsUpdated(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum);
+  }
 }
